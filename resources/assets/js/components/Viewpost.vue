d917c678b703051ec72848dc9c5e98e3cc4a39ff
Miguel Rodríguez <miguel@rodriguez.me>
2018-05-21 02:53:16 +0200

    Initial configuration

---
+++ resources/assets/js/components/Viewpost.vue
@@ -0,0 +1,24 @@
+<template id="post">
+    <div>
+        <h2>{{ post.name }}</h2>
+        <b>Description: </b>
+        <div>{{ post.description }}</div>
+        <br/>
+        <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+        <router-link v-bind:to="'/'">Back Post list</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {post: {name: '', description: ''}}
+    },
+    created: function(){
+        let uri = 'http://vue-crud.local/api/'+this.$route.params.id;
+        Axios.get(uri).then((response) => {
+            this.post = response.data;
+        });
+    }
+}
+</script>
